452340a6ed85cd670ed334ddc5bd12a70d45baca
Harsh Patni <hpatni3@gmail.com>
2018-01-20 15:15:30 +0530

    Spring tiles

---
+++ SpringTiles/src/com/javatpoint/form/Contact.java
@@ -0,0 +1,9 @@
+package com.javatpoint.form;
+
+public class Contact {  
+    private String firstname;  
+    private String lastname;  
+    private String email;  
+    private String telephone;  
+    //getters and setters  
+} 
